f768708c5ae4362b8b8f8cc39efc817c8995294a
忧 幻梦 <Zhang15063075175@outlook.com>
2022-02-18 20:26:57 +0800

    添加项目文件。

---
+++ 早苗日刷工具/GetConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 早苗机器人日刷工具
+{
+    /// <summary>
+    /// 此处负责创建/读取config，用户可在其中进行一系列的设置
+    /// </summary>
+    public class GetConfig
+    {
+        static void OnInitializing()
+        {
+            string pathConfig = @".//config.json";
+            if (File.Exists(pathConfig) == true)
+            {
+                string ReadConfig = File.ReadAllText(pathConfig);
+            }
+            else
+            {
+                File.Create(@".//config,json");
+            }
+        }
+
+    }
+}
---
+++ 早苗日刷工具/Properties.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 早苗机器人日刷工具
+{
+    internal class Properties
+    {
+        //输入的内容
+        //
+    }
+}
